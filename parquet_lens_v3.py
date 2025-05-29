dc5e0247bad7a2151c021ab6612201ed9a0a2aa1
Chungmin Lee <chungminlee@microsoft.com>
2025-05-29 01:39:42 +0000

    v3

--- parquet_lens_v3.py
+++ parquet_lens_v3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import json
 import logging
@@ -97,7 +98,7 @@ class OffsetRecordingProtocol(TProtocol.TProtocolBase):
             "value": [],
         }
 
-    def get_info(self):
+    def get_offset_info(self):
         return self._current
 
     def readStructBegin(self):
@@ -393,16 +394,20 @@ def read_thrift_segment(f, offset, name, thrift_class):
     )
     obj = thrift_class()
     obj.read(protocol)
-    segment = create_segment_from_offset_info(protocol.get_info(), base_offset=offset)
+    segment = create_segment_from_offset_info(
+        protocol.get_offset_info(), base_offset=offset
+    )
     return obj, segment
 
 
 def read_pages(f, column_chunk, segments):
     remaining_values = column_chunk.meta_data.num_values
     offset = column_chunk.meta_data.data_page_offset
+    offsets = []
     while remaining_values > 0:
         page, page_segment = read_thrift_segment(f, offset, "page", PageHeader)
         page_header_end = page_segment["offset"] + page_segment["length"]
+        offsets.append(page_segment["offset"])
         segments.append(page_segment)
         segments.append(
             create_segment(
@@ -419,6 +424,7 @@ def read_pages(f, column_chunk, segments):
             break
         remaining_values -= num_values
         offset = page_header_end + page.compressed_page_size
+    return offsets
 
 
 def read_dictionary_page(f, column_chunk, segments):
@@ -438,13 +444,7 @@ def read_dictionary_page(f, column_chunk, segments):
             "page_data",
         )
     )
-
-
-def read_offset_index(f, column_chunk, segments):
-    _, offset_index_segment = read_thrift_segment(
-        f, column_chunk.offset_index_offset, "offset_index", OffsetIndex
-    )
-    segments.append(offset_index_segment)
+    return dict_page_segment["offset"]
 
 
 def read_column_index(f, column_chunk, segments):
@@ -452,6 +452,15 @@ def read_column_index(f, column_chunk, segments):
         f, column_chunk.column_index_offset, "column_index", ColumnIndex
     )
     segments.append(column_index_segment)
+    return column_index_segment["offset"]
+
+
+def read_offset_index(f, column_chunk, segments):
+    _, offset_index_segment = read_thrift_segment(
+        f, column_chunk.offset_index_offset, "offset_index", OffsetIndex
+    )
+    segments.append(offset_index_segment)
+    return offset_index_segment["offset"]
 
 
 def read_bloom_filter(f, column_chunk, segments):
@@ -459,6 +468,7 @@ def read_bloom_filter(f, column_chunk, segments):
         f, column_chunk.bloom_filter_offset, "bloom_filter", BloomFilterHeader
     )
     segments.append(bloom_filter_segment)
+    return bloom_filter_segment["offset"]
 
 
 def fill_gaps(segments, file_size):
@@ -472,6 +482,76 @@ def fill_gaps(segments, file_size):
     if offset != file_size:
         new_segments.append(create_segment(offset, file_size, "unknown"))
     return new_segments
+
+
+def parse_parquet_file(file_path):
+    segments = []
+
+    with open(file_path, "rb") as f:
+        # Read file header
+        f.seek(0)
+        header = f.read(4)
+        if header != b"PAR1":
+            raise ValueError("Not a valid Parquet file - missing PAR1 header")
+        segments.append(create_segment(0, 4, "magic_number", "PAR1"))
+
+        # Read footer length (last 8 bytes)
+        f.seek(-8, 2)
+        footer_size = f.read(4)
+        footer_magic = f.read(4)
+        file_size = f.tell()
+        if footer_magic != b"PAR1":
+            raise ValueError("Not a valid Parquet file - missing PAR1 footer")
+        footer_size = struct.unpack("<I", footer_size)[0]
+        segments.append(
+            create_segment(file_size - 4, file_size, "magic_number", "PAR1")
+        )
+        segments.append(
+            create_segment(file_size - 8, file_size - 4, "footer_length", footer_size)
+        )
+
+        # Parse footer with offset recording
+        footer_offset = file_size - 8 - footer_size
+        footer, footer_segment = read_thrift_segment(
+            f, footer_offset, "footer", FileMetaData
+        )
+        segments.append(footer_segment)
+
+        column_chunk_data_offsets = {}
+
+        for row_group in footer.row_groups:
+            for column_chunk in row_group.columns:
+                column_key = tuple(column_chunk.meta_data.path_in_schema)
+                offset_list = column_chunk_data_offsets.setdefault(column_key, [])
+
+                offsets = {}
+                offsets["data_pages"] = read_pages(f, column_chunk, segments)
+
+                if column_chunk.meta_data.dictionary_page_offset is not None:
+                    offsets["dictionary_page"] = read_dictionary_page(
+                        f, column_chunk, segments
+                    )
+
+                if column_chunk.column_index_offset is not None:
+                    offsets["column_index"] = read_column_index(
+                        f, column_chunk, segments
+                    )
+
+                if column_chunk.offset_index_offset is not None:
+                    offsets["offset_index"] = read_offset_index(
+                        f, column_chunk, segments
+                    )
+
+                if column_chunk.meta_data.bloom_filter_offset is not None:
+                    offsets["bloom_filter"] = read_bloom_filter(
+                        f, column_chunk, segments
+                    )
+
+                offset_list.append(offsets)
+
+    segments.sort(key=lambda s: s["offset"])
+    segments = fill_gaps(segments, file_size)
+    return segments, column_chunk_data_offsets
 
 
 def segment_to_json(segment):
@@ -490,66 +570,125 @@ def segment_to_json(segment):
     return segment
 
 
-def parse_parquet_file(file_path):
-    segments = []
+def find_footer_segment(segments):
+    for s in segments:
+        if s["name"] == "footer":
+            return s
 
-    with open(file_path, "rb") as f:
-        # Read file header
-        f.seek(0)
-        header = f.read(4)
-        if header != b"PAR1":
-            raise ValueError("Not a valid Parquet file - missing PAR1 header")
-        segments.append(create_segment(0, 4, "magic_number", "PAR1"))
 
-        # Read footer length (last 8 bytes)
-        f.seek(-8, 2)
-        footer_size_bytes = f.read(4)
-        footer_magic = f.read(4)
-        file_size = f.tell()
-        if footer_magic != b"PAR1":
-            raise ValueError("Not a valid Parquet file - missing PAR1 footer")
-        footer_size = struct.unpack("<I", footer_size_bytes)[0]
-        segments.append(
-            create_segment(file_size - 4, file_size, "magic_number", "PAR1")
-        )
-        segments.append(
-            create_segment(file_size - 8, file_size - 4, "footer_length", footer_size)
-        )
+def get_summary(footer, segments):
+    summary = {}
+    summary["num_rows"] = footer["num_rows"]
+    summary["num_row_groups"] = len(footer["row_groups"])
+    if footer["row_groups"]:
+        summary["num_columns"] = len(footer["row_groups"][0]["columns"])
 
-        # Parse footer with offset recording
-        footer_offset = file_size - 8 - footer_size
-        footer, footer_segment = read_thrift_segment(
-            f, footer_offset, "footer", FileMetaData
-        )
-        segments.append(footer_segment)
+    num_pages = 0
+    num_data_pages = 0
+    num_v1_data_pages = 0
+    num_v2_data_pages = 0
+    num_dict_pages = 0
+    page_header_size = 0
+    uncompressed_page_data_size = 0
+    compressed_page_data_size = 0
+    for s in segments:
+        if s["name"] == "page":
+            num_pages += 1
+            page_header_size += s["length"]
+            page_json = segment_to_json(s)
+            if page_json["type"] in ("DATA_PAGE", "DATA_PAGE_V2"):
+                num_data_pages += 1
+            elif page_json["type"] == "DICTIONARY_PAGE":
+                num_dict_pages += 1
+            if "data_page_header" in page_json:
+                num_v1_data_pages += 1
+            if "data_page_header_v2" in page_json:
+                num_v2_data_pages += 1
+            uncompressed_page_data_size += page_json["uncompressed_page_size"]
+            compressed_page_data_size += page_json["compressed_page_size"]
 
-        for row_group in footer.row_groups:
-            for column_chunk in row_group.columns:
-                read_pages(f, column_chunk, segments)
+    summary["num_pages"] = num_pages
+    summary["num_data_pages"] = num_data_pages
+    summary["num_v1_data_pages"] = num_v1_data_pages
+    summary["num_v2_data_pages"] = num_v2_data_pages
+    summary["num_dict_pages"] = num_dict_pages
 
-                if column_chunk.meta_data.dictionary_page_offset is not None:
-                    read_dictionary_page(f, column_chunk, segments)
+    # Sum of page header sizes for all pages in the file
+    summary["page_header_size"] = page_header_size
+    summary["uncompressed_page_data_size"] = uncompressed_page_data_size
+    summary["compressed_page_data_size"] = compressed_page_data_size
 
-                if column_chunk.offset_index_offset is not None:
-                    read_offset_index(f, column_chunk, segments)
+    uncompressed_page_size = 0
+    compressed_page_size = 0
+    column_index_size = 0
+    offset_index_size = 0
+    bloom_fitler_size = 0
+    for row_group in footer["row_groups"]:
+        for column in row_group["columns"]:
+            uncompressed_page_size += column["meta_data"]["total_uncompressed_size"]
+            compressed_page_size += column["meta_data"]["total_compressed_size"]
+            column_index_size += column.get("column_index_length", 0)
+            offset_index_size += column.get("offset_index_length", 0)
+            bloom_fitler_size += column.get("bloom_filter_length", 0)
 
-                if column_chunk.column_index_offset is not None:
-                    read_column_index(f, column_chunk, segments)
+    # These page sizes include header size
+    summary["uncompressed_page_size"] = uncompressed_page_size
+    summary["compressed_page_size"] = compressed_page_size
 
-                if column_chunk.meta_data.bloom_filter_offset is not None:
-                    read_bloom_filter(f, column_chunk, segments)
+    summary["column_index_size"] = column_index_size
+    summary["offset_index_size"] = offset_index_size
+    summary["bloom_fitler_size"] = bloom_fitler_size
 
-    segments.sort(key=lambda s: s["offset"])
-    segments = fill_gaps(segments, file_size)
-    return {
-        "segments": segments,
-        "footer": segment_to_json(footer_segment),
-    }
+    footer = find_footer_segment(segments)
+    if footer is not None:
+        summary["footer_size"] = footer["length"]
+    summary["file_size"] = segments[-1]["offset"] + segments[-1]["length"]
+
+    return summary
+
+
+def get_pages(segments, column_chunk_data_offsets):
+    page_offset_map = {}
+    for s in segments:
+        if s["name"] in ("page", "column_index", "offset_index", "bloom_filter"):
+            page_offset_map[s["offset"]] = segment_to_json(s)
+    column_pages = []
+
+    def with_offset(offset):
+        obj = {"$offset": offset}
+        obj.update(page_offset_map[offset])
+        return obj
+
+    for col_idx, (column_path, offsets) in enumerate(column_chunk_data_offsets.items()):
+        pages = {"$index": col_idx, "column": column_path}
+        row_groups = []
+        for row_group_idx, offset_info in enumerate(offsets):
+            row_group = {"$index": row_group_idx}
+            if offset_info.get("dictionary_page"):
+                row_group["dictionary_page"] = with_offset(
+                    offset_info["dictionary_page"]
+                )
+            if offset_info.get("data_pages"):
+                data_pages = []
+                for offset in offset_info["data_pages"]:
+                    data_pages.append(with_offset(offset))
+                row_group["data_pages"] = data_pages
+            if offset_info.get("column_index"):
+                row_group["column_index"] = with_offset(offset_info["column_index"])
+            if offset_info.get("offset_index"):
+                row_group["offset_index"] = with_offset(offset_info["offset_index"])
+            if offset_info.get("bloom_filter"):
+                row_group["bloom_filter"] = with_offset(offset_info["bloom_filter"])
+            row_groups.append(row_group)
+        pages["row_groups"] = row_groups
+        column_pages.append(pages)
+    return column_pages
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("parquet_file")
+    parser.add_argument("-s", "--show-offsets-and-thrift-details", action="store_true")
     parser.add_argument("--log-level", default="INFO")
     args = parser.parse_args()
 
@@ -559,8 +698,17 @@ def main():
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    segments = parse_parquet_file(args.parquet_file)
-    print(json.dumps(segments, indent=2))
+    segments, column_chunk_data_offsets = parse_parquet_file(args.parquet_file)
+    if args.show_offsets_and_thrift_details:
+        output = segments
+    else:
+        footer = segment_to_json(find_footer_segment(segments))
+        output = {
+            "summary": get_summary(footer, segments),
+            "footer": footer,
+            "pages": get_pages(segments, column_chunk_data_offsets),
+        }
+    print(json.dumps(output, indent=2))
 
 
 if __name__ == "__main__":
